3d19f1e681dedc4ae0a589e622a0b0480b1e3d2b
Pavan <pavan@chingari.io>
2022-06-16 11:04:51 +0530

    Added extra convenience init to handle custom containerview.

--- Sources/MaterialShowcase/MaterialShowcase.swift
+++ Sources/MaterialShowcase/MaterialShowcase.swift
@@ -325,15 +325,6 @@ extension MaterialShowcase {
         containerView = window
         setDefaultProperties()
     }
-    
-    public func configure() {
-        backgroundColor = UIColor.clear
-        guard let window = UIApplication.shared.keyWindow else {
-            return
-        }
-        containerView = window
-        setDefaultProperties()
-    }
 
   func setDefaultProperties() {
     // Background
@@ -709,17 +700,12 @@ extension MaterialShowcase {
 
 extension MaterialShowcase {
     
-    public init(customContainerView: UIView) {
-        // Create frame
-        let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-        super.init(frame: frame)
-        
+    public convenience init(customContainerView: UIView) {
+        self.init()
         configureContainerView(customContainerView)
     }
     
     func configureContainerView(_ containerView: UIView) {
-        backgroundColor = UIColor.clear
-        containerView = containerView
-        setDefaultProperties()
+        self.containerView = containerView
     }
 }
